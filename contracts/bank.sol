4bf8e8226e696e70c6e66e2cdfc4d72df5f40b40
duytacong24895 <duytacong.64b@gmail.com>
2025-03-05 17:28:53 +0700

    betav1:bank

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -2,36 +2,44 @@
 pragma solidity 0.8.26;
 
 contract Bank {
-    mapping(address => uint256) private _ledger;
-    struct customer {
-        address addr;
-        string name;
-    }
-    mapping(address => customer) private _customers;
+    mapping(address => uint256) private _balances;
+    mapping(address => bool) private  _userExists;
 
     uint256 public balance = 0;
 
-    function cashIn() public  payable  {
-        
+    function cashIn() public payable  {
+        require(checkUserExists(msg.sender), "You do not exist");
+        _balances[msg.sender] += msg.value;
     }
 
-    function newCustomer(string memory name) public  {
-        address sender = msg.sender;
-        require(_customers[msg.sender].addr == address(0), "You are already a customer.");
-        _customers[sender] = customer(msg.sender, name);
+    function cashOut(uint256 amount) public payable  {
+        require(checkUserExists(msg.sender), "You do not exist");
+        require(_balances[msg.sender] >= amount);
+        _balances[msg.sender] = _balances[msg.sender] - amount;
+        payable(msg.sender).transfer(amount);
     }
 
-    function checkCustomer() private view returns(bool isInit) {
-        if(_customers[msg.sender].addr == address(0)) {
-            return false;
-        }
-        return true;
+    function getBalance() public view returns (uint256) {
+        return _balances[msg.sender];
     }
-    
-    // function deposit() public payable {
-    //     address sender = msg.sender;
-    //     require(_ledger[sender] == 0, "You are already depositing.");
-    //     _ledger[sender] = msg.value;
-    //     balance += msg.value;
-    // }
+
+    function getContractBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+    function transfer(address payable _to, uint256 amount) public payable  {
+        require(amount == msg.value, "Please check your amount!");
+        require(checkUserExists(_to));
+        require(isEnoughBalance(amount), "You don't have enough balance, Please deposit in more!");
+        _to.transfer(msg.value);
+
+    }
+
+    function checkUserExists(address addr) private view returns(bool) {
+        return _userExists[addr];
+    }
+
+    function isEnoughBalance(uint256 amount) public  view returns(bool) {
+        return _balances[msg.sender] >= amount;
+    }
 }
